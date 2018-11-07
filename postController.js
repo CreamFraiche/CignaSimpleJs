e15e74b2df5b8895c7bd34a4247173c300aeee86
CreamFraiche <sama93536@gmail.com>
2018-11-06 19:50:34 -0500

    Added selection functionality
    One can now select between all, odd, even, to only see certain posts. Added a clicking function to help visual differences.

--- postController.js
+++ postController.js
@@ -1,1 +1,54 @@
-{\rtf1}
+$(document).ready(function(){
+
+	//I added this so one can more easily track the posts
+	$(document).on('click', ".post", function(){
+		$(this).toggleClass('selected');
+	});
+
+	//Parse select input
+	$( "#target" ).change(function() {
+		showAll();
+		var selection = $('#target').val();
+
+		switch(selection){
+			case "all":
+				selectAll();
+				break;
+			case "odd":
+				selectOdd();
+				break;
+			case "even":
+				selectEven();
+				break;
+
+		}
+		
+	});
+
+});
+
+showAll = function(){
+	$(".post-wrapper").show();
+}
+
+selectAll = function(){
+	$(".post-wrapper").show();
+}
+
+selectEven = function(){
+	$(".post-wrapper").each(function(){
+		var Id = $(this).attr('id');
+		if(Id % 2 != 0){
+			$(this).slideToggle("fast");
+		}
+	});
+}
+
+selectOdd = function(){
+	$(".post-wrapper").each(function(){
+		var Id = $(this).attr('id');
+		if(Id % 2 != 1){
+			$(this).slideToggle("fast");
+		}
+	});
+}
